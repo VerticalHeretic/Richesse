be7cbf7db3c1732ecc2f2a560795e925ceafb8b1
Łukasz Stachnik <ls.warss98@gmail.com>
2024-04-23 15:05:25 +0200

    Initial Commit

---
+++ Richesse/RichesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichesseApp.swift
+//  Richesse
+//
+//  Created by Łukasz Stachnik on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RichesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
